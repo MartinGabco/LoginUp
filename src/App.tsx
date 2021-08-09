2fc98317f861a9ad79ad0e97c054b535d4809150
MartinGabco <gabcomartin@gmail.com>
2021-08-09 13:33:04 +0200

    Views components created

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
 
+//views
+import Introduction from './views/Introduction';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <Introduction/>
+      </header>
     </div>
   );
 }
---
+++ src/views/Login.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Login: React.FC = () => {
+    return ( <p>Login</p> );
+}
+ 
+export default Login;
