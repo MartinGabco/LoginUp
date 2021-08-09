56457c50b3d7069fa871a6ea5ecaa7712108d154
MartinGabco <gabcomartin@gmail.com>
2021-08-09 18:58:22 +0200

    submitHandler

--- src/views/Registration.tsx
+++ src/views/Registration.tsx
@@ -6,24 +6,57 @@ import { Link } from 'react-router-dom';
 //styles
 import '../styles/Registration.css';
 
-const Registration: React.FC = () => ( 
+const Registration: React.FC = () => {
+
+    const submitHandler = (event: React.FormEvent) => {
+        event.preventDefault();
+        console.log('It runs!TS works!');
+    }
+
+    return (
         <div className="registration">
             <p className="rtitle">Registration</p>
             <Link to="/login">I am registered, I want to login</Link>
             <section className="form-place">
-                <form>
+                <form onSubmit={submitHandler}>
                     <div className="email">
-                        <label htmlFor="email" className="email-title">Your Email</label>
-                        <input type="email" id="email" className="input" required/>
+                        <label 
+                            htmlFor="email" 
+                            className="email-title"
+                        >
+                            Your Email
+                        </label>
+                        <input 
+                            type="email" 
+                            id="email" 
+                            className="input" 
+                            required
+                        />
                     </div>
                     <div className="password">
-                        <label htmlFor="password" className="password-title">Your password</label>
-                        <input type="password" id="password" className="input" required/>
+                        <label 
+                            htmlFor="password" 
+                            className="password-title"
+                        >
+                            Your password
+                        </label>
+                        <input 
+                            type="password" 
+                            id="password" 
+                            className="input" 
+                            required
+                        />
                     </div>
-                    <button type="button" className="submit-button">Register me, please!</button>
+                    <button 
+                        type="submit" 
+                        className="submit-button"
+                    >
+                        Register me, please!
+                    </button>
                 </form>
             </section>
         </div>
     )
+}
  
 export default Registration;
