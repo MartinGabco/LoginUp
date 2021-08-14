a3fd9a12a2151d5a20265bf797d7555366e34607
MartinGabco <gabcomartin@gmail.com>
2021-08-14 19:07:15 +0200

    Login form established

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -5,12 +5,51 @@ import { Link } from 'react-router-dom';
 
 //styles
 import '../styles/Login.css';
+//styles
+import '../styles/Registration.css';
 
 const Login: React.FC = () => {
     return ( 
         <div className="login">
             <p className="ltitle">Login</p>
-            <Link to="/home">Log in</Link>
+            <section className="form-place">
+                <form>
+                    <div className="email">
+                        <label 
+                            htmlFor="email" 
+                            className="email-title"
+                        >
+                            Your Email
+                        </label>
+                        <input 
+                            type="email" 
+                            id="email" 
+                            className="input" 
+                            required
+                        />
+                    </div>
+                    <div className="password">
+                        <label 
+                            htmlFor="password" 
+                            className="password-title"
+                        >
+                            Your password
+                        </label>
+                        <input 
+                            type="password" 
+                            id="password" 
+                            className="input" 
+                            required
+                        />
+                    </div>
+                    <button 
+                        type="submit" 
+                        className="submit-button"
+                    >
+                        LOG IN!
+                    </button>
+                </form>
+            </section>
         </div>
     );
 }
